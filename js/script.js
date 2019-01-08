088e46861808fbe66ac96b3c388df6e9b3695ea3
Matthias Rudin <matthias.rudin@live.com>
2019-01-08 20:35:23 +0100

    Added comments

--- js/script.js
+++ js/script.js
@@ -2,33 +2,31 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+/************************************************************************************
+Global variables / constants
+************************************************************************************/
 // List of all students
 const studentNodeList = document.querySelectorAll('li');
 const pageContainer = document.querySelector('.page');
 
-// Constant for number of items per page (can be changed dynamically)
+// Constant for number of items per page
+// Naming convention comes from the Java programming language
 const ITEMS_PER_PAGE = 10;
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+/************************************************************************************
+showPage function
+************************************************************************************/
+// The bellow function is a primary function and sets the number of item per page
+const showPage = () => {
+   const activeButton = document.querySelector('a[class="active"');
+   const pageNumber = activeButton.innerText;
+   showPageWithItems(pageNumber);
+}
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-const showPage = (pageNumber) => {
+// The bellow function displays only the current active items
+const showPageWithItems = pageNumber => {
    const startIndex = calculateStartIndex(pageNumber);
    const endIndex = startIndex + ITEMS_PER_PAGE;
    for(let i = 0; i < studentNodeList.length; i++) {
@@ -40,33 +38,44 @@ const showPage = (pageNumber) => {
    }
 }
 
+// The following function calculates the startindex
 const calculateStartIndex = pageNumber => Number(pageNumber + 0) - ITEMS_PER_PAGE;
 
-const onInitialization = () => {
-   const activeButton = document.querySelector('a[class="active"');
-   const pageNumber = activeButton.innerText;
-   showPage(pageNumber);
+
+
+/************************************************************************************
+appendPageLinks function
+************************************************************************************/
+// The following function is a primary function. Its taks is to create and append the pagination element
+const appendPageLinks = () => {
+   const pagination = createPagination();
+   pageContainer.appendChild(pagination);
 }
 
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-const appendPageLinks = () => {
+// The bellow function creates the pagination element and adds functionality to it
+const createPagination = () => {
    const divContainer = createElementWithClass('div', 'pagination')
    const ulContainer = document.createElement('ul');
    const numOfPages = calculatePageNumbers(studentNodeList, ITEMS_PER_PAGE);
-
    for(let i = 1; i <= numOfPages; i++) {
       createAndAppend('li', 'a', i, '#', ulContainer)   
    }
-
    divContainer.appendChild(ulContainer);
+   divContainer.addEventListener('click', event => {
+      const anchorList = document.querySelectorAll('a[href="#"]');
+      for(let i = 0; i < anchorList.length; i++) {
+         anchorList[i].className = '';
+      }
+      const button = event.target;
+      button.className = 'active'
+      const pageNumber = button.innerText;
+      showPageWithItems(pageNumber);
+   })
    return divContainer;
 }
 
-// The following function returns the number of pages. numPerPage is dynamic and can be changed according requirements
+
+// The following function returns the number of pages
 const calculatePageNumbers = (nodeList, numPerPage) => Math.ceil(nodeList.length / numPerPage);
 
 // The following function will create two elements, add a given text and href to the second element, and append the elements as childs to a third element
@@ -75,7 +84,7 @@ const createAndAppend = (firstElement, secondElement, secondTextContent, secondH
    const second = document.createElement(secondElement);
    second.innerText = secondTextContent;
    second.href = secondHRef;
-   // The following line sets the page-button at initiation active, if the number is 1
+   // The following line sets the page-button at initialization active, if the page number is 1 (at the first load of the page)
    if(secondTextContent === 1) second.className ='active';
    first.appendChild(second);
    appendToElemend.appendChild(first); 
@@ -88,17 +97,9 @@ const createElementWithClass = (elementName, className) => {
    return element;
 }
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
-const pagination = appendPageLinks();
-pagination.addEventListener('click', event => {
-   const anchorList = document.querySelectorAll('a[href="#"]');
-   for(let i = 0; i < anchorList.length; i++) {
-      anchorList[i].className = '';
-   }
-   const button = event.target;
-   button.className = 'active'
-   const pageNumber = button.innerText;
-   showPage(pageNumber);
-})
-pageContainer.appendChild(pagination);
-onInitialization();
+
+/************************************************************************************
+Invokation of both primary functions
+************************************************************************************/
+appendPageLinks();
+showPage();
